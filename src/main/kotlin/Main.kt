c38a59838d148081139934df998eef5ae2f2fad9
Alice <hifumialice@gmail.com>
2024-04-25 10:38:18 +0900

    메인 í클래스 진행

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,19 @@
 package org.example
+import java.util.Scanner
+
 
 fun main() {
-    println("Hello World!")
+    val cal = Calculator()
+    val scanner = Scanner(System.`in`)
+
+    var numberLong : Long = 0
+    var numberDouble : Double = 0.0
+    var resultLong : Long = 0
+    var resultDouble : Double = 0.0
+
+    var operator : String = " "
+
+
+
+
 }
