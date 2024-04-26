e6024a2a7f4d8ceb80cab742c93d56e15f826f06
Alice <hifumialice@gmail.com>
2024-04-26 16:44:50 +0900

    0체크를 추상화에 넣고 나누기 나머지 클래스에서 제거

--- src/main/kotlin/AbstractCalculate.kt
+++ src/main/kotlin/AbstractCalculate.kt
@@ -2,4 +2,20 @@ package org.example
 
 abstract class AbstractCalculate {
     abstract fun calculate(num1: Any, num2: Any) : Any
+
+    fun zeroCheck(n : Number) : Boolean {
+
+        if (n is Long || n is Double) {
+            if (n == 0L || n == 0.0) {
+                println("------------------------------------")
+                println("0으로 나눌 수 없습니다.")
+                println("다시 입력해주세요")
+                println("------------------------------------")
+                return true
+            } else {
+                return false
+            }
+        }
+        return true
+    }
 }
--- src/main/kotlin/Divide.kt
+++ src/main/kotlin/Divide.kt
@@ -45,27 +45,8 @@ class Divide : AbstractCalculate() {
         }
     }
 
-    private fun zeroCheck(n : Long) : Boolean {
-        if (n == 0.toLong()) {
-            println("------------------------------------")
-            println("0으로 나눌 수 없습니다.")
-            println("다시 입력해주세요")
-            println("------------------------------------")
-            return true
-        } else {
-            return false
-        }
-    }
 
-    private fun zeroCheck(n : Double) : Boolean {
-        if (n == 0.0) {
-            println("------------------------------------")
-            println("0으로 나눌 수 없습니다.")
-            println("다시 입력해주세요")
-            println("------------------------------------")
-            return true
-        } else {
-            return false
-        }
-    }
+
+
+
 }
